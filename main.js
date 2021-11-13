30ed5c7bc05bfff4c81522a6db832a475f493697
Rafael Álvarez <rafaelalma92@gmail.com>
2021-11-13 17:26:50 +0100

    Second draft of calculator functionality, add clear feature

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function multiply(operand1, operand2) {
 
 function divide(operand1, operand2) {
   if (operand2 == 0) {
-    throw new Error("Can't divide by zero");
+    throw new Error(DIVIDE_BY_ZERO_ERROR);
   }
 
   return operand1 / operand2;
@@ -31,7 +31,12 @@ function operate(operator, operand1, operand2) {
     case "*":
       return multiply(operand1, operand2);
     case "/":
-      return divide(operand1, operand2);
+      try {
+        return divide(operand1, operand2);
+      } catch (error) {
+        console.log(error);
+        return 0;
+      }
     default:
       throw new Error("Invalid operator");
   }
@@ -45,11 +50,39 @@ function updateResult(value) {
   result.textContent = value;
 }
 
+function parseOperation() {
+  [operand1Value, operand2Value] = memoryValue.split(operatorValue);
+
+  if (operand2Value === "") return;
+
+  resultValue = formatNumber(
+    operate(operatorValue, operand1Value, operand2Value)
+  ).toString();
+  updateResult(resultValue);
+
+  memoryValue = resultValue;
+  resultValue = "";
+  operatorValue = "";
+  operand1Value = "";
+  operand2Value = "";
+
+  canWriteNumber = false;
+}
+
+// TODO: Refactor
+// BUG: After second input operator, perform calculation, but don't save the operator
+// TODO: Float
+// TODO: Delete
+// TODO: Keyboard support
+
+const DIVIDE_BY_ZERO_ERROR = "Can't divide by zero";
+
 const memory = document.querySelector(".memory");
 const result = document.querySelector(".result");
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 const equals = document.querySelector(".equals");
+const clear = document.querySelector(".clear");
 
 let memoryValue = "";
 let operatorValue = "";
@@ -57,9 +90,11 @@ let operand1Value = "";
 let operand2Value = "";
 let resultValue = "";
 
+let canWriteNumber = true;
+
 numbers.forEach((number) => {
   number.addEventListener("click", (event) => {
-    if (resultValue !== "") return;
+    if (!canWriteNumber) return;
 
     memoryValue += event.currentTarget.textContent;
     updateMemory(memoryValue);
@@ -68,7 +103,15 @@ numbers.forEach((number) => {
 
 operators.forEach((operator) => {
   operator.addEventListener("click", (event) => {
-    if (memoryValue === "" || operatorValue !== "") return;
+    if (memoryValue === "") return;
+
+    canWriteNumber = true;
+
+    if (operatorValue !== "") {
+      parseOperation();
+
+      return;
+    }
 
     operatorValue = event.currentTarget.textContent;
 
@@ -80,16 +123,17 @@ operators.forEach((operator) => {
 equals.addEventListener("click", () => {
   if (memoryValue === "" || operatorValue === "") return;
 
-  [operand1Value, operand2Value] = memoryValue.split(operatorValue);
+  parseOperation();
+});
 
-  if (operand2Value === "") return;
-
-  resultValue = operate(operatorValue, operand1Value, operand2Value).toString();
-  updateResult(resultValue);
-
-  memoryValue = resultValue;
-  resultValue = "";
+clear.addEventListener("click", () => {
+  memoryValue = "";
   operatorValue = "";
   operand1Value = "";
   operand2Value = "";
+  resultValue = "";
+  canWriteNumber = true;
+
+  updateMemory("");
+  updateResult("");
 });
